df45f65f3bf1733bf60c7c1c832b861ac4b7e2a0
Hümeysa Aktaş <81477253+Humeysa@users.noreply.github.com>
2022-06-10 09:51:40 +0300

    Add project files.

---
+++ DataAccsessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccsessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        // birer tablo kısmı olarak yer alacak
+        //About : Projemizin içerisinde yer alan Classımızın ismidir.
+        //Abouts : SQL de veritabanımıza yansıyacak olan tablomuzun ismidir.
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        //buraya yazılmış olan dbset türündeki propertyleri sql birer tablo olarak yansıtacak
+    }
+}
